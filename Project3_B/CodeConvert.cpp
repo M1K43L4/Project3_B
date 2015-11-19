53461175da47b449363d9b3818122a359dc5a4ee
beauxq <beauxq@users.noreply.github.com>
2015-11-19 17:09:12 -0600

    edits
    renamed Map to make it more clear (also, non type names should be lower
    case)
    
    removed the unneeded "junk" entry, can just return an empty map
    
    condensed data reading code
    
    added some comments

--- Project3_B/CodeConvert.cpp
+++ Project3_B/CodeConvert.cpp
@@ -3,32 +3,28 @@
 #include <iostream>
 
 std::map<std::string, char> CodeConvert::load_from_file(){
-	
-	std::map<std::string, char> Map;
+
+	std::map<std::string, char> map_to_return;
 	std::ifstream fin("morse.txt");
 
-	if (!fin)//if fin didn't open
-	{
+	if (! (fin.good() && fin.is_open())) {  //if fin didn't open
 		// 'morse.txt' file not found.
 
 		//return a junk map to appease the return value of the function
-		std::string junk;
-		char j = 'j';
-		Map[junk] = j;
-		return Map;
+		//std::string junk;
+		//char j = 'j';
+		//Map[junk] = j;
+		return map_to_return;
 	}
 
-	std::string key;
-	char value;
-	while (fin){
-		fin >> value;
-		fin >> key;
+	std::string key;  // code of dots and dashes
+	char value;       // character represented by code
 
-		Map[key] = value;
+	while (fin >> value >> key){
+		map_to_return[key] = value;
 	}
 
-		
 	fin.close();
-	return Map;
 
+	return map_to_return;
 }
