1189241cd1aea80933c62630c0fd37d6160c045c
beauxq <beauxq@user.noreply.github.com>
2015-12-04 09:49:19 -0600

    more clean up removed some todo notes put in more tests (so we're not just testing one letter)

--- Project3_B/CodeConvert.cpp
+++ Project3_B/CodeConvert.cpp
@@ -23,7 +23,6 @@ bool CodeConvert::load_map_from_file(std::map<std::string, CharData>& map_for_tr
 	std::string code;  // code of dots and dashes
 	CharData value;    // struct used as value in map_for_tree_creation
 
-
 	while (fin >> value.character >> code){
         value.dot_count = std::count(code.begin(), code.end(), DOT);
 		map_for_tree_creation[code] = value;
@@ -45,7 +44,7 @@ void CodeConvert::create_vector(std::vector<char>& vector_for_tree_creation,
     for (auto itr = map_for_tree_creation.begin(); itr != map_for_tree_creation.end(); ++itr){
         // add to the vector, nulls, numbering (the number of dots removed from the previous code) + 1
         dots_removed_plus_one = previous_code_dot_count + 1 - (itr->second).dot_count;
-        // TODO: error if this number is less than zero? (added more than one dot)
+        // note: if this number is less than zero, the data is not valid and tree will be created incorrectly
         for (; dots_removed_plus_one > 0; --dots_removed_plus_one){
             vector_for_tree_creation.push_back(0);
         }
@@ -62,20 +61,10 @@ void CodeConvert::load_from_file(){
     std::vector<char> vector_for_tree_creation;
 
     load_map_from_file(map_for_tree_creation);
-    // TODO: check for return false for file open error
 
     create_vector(vector_for_tree_creation, map_for_tree_creation);
 
-    // test, TODO: remove tests?
-    // does it put it in the right order like we thought it would?
-    for (auto itr = vector_for_tree_creation.begin(); itr != vector_for_tree_creation.end(); ++itr)
-        std::cout << *itr << std::endl;
-
     decode_tree.read_tree(vector_for_tree_creation);
-
-    // test
-    std:: cout << decode_tree.to_string();
-
 }
 
 std::string CodeConvert::decode(std::string morse_code){
--- Project3_B/main.cpp
+++ Project3_B/main.cpp
@@ -6,13 +6,28 @@ void test(){
     CodeConvert a;
     a.load_from_file();
 
-	std::cout << std::endl << std::endl;
+	std::cout << "/ntesting decode:\n";
 
 	std::cout << a.decode("..._ ..._") << std::endl;
-	std::cout << a.decode(".......") << std::endl;
+	std::cout << a.decode("_.. ___ .._ __.") << std::endl;
+	std::cout << a.decode("__ .. _._ ._ . ._.. ._") << std::endl;
+	std::cout << a.decode(".___ . ... ... .. _._. ._") << std::endl;
+
+	std::cout << "\ntesting decode error:\n";
+
+	std::cout << a.decode(".......");
+
+	std::cout << "\ntesting encode:\n";
+
+	std::cout << a.encode("hoskisson") << std::endl;
+	std::cout << a.encode("cubon") << std::endl;
+	std::cout << a.encode("sevillapulido") << std::endl;
 	std::cout << a.encode("vv") << std::endl;
-	std::cout << a.encode("v v") << std::endl;
-	std::cout << a.encode("?") << std::endl;
+
+	std::cout << "\ntesting encode error:\n";
+
+	std::cout << a.encode("v v");
+	std::cout << a.encode("?");
 }
 
 int main(){
